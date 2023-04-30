db823b33abd9e22d965c42e32e01f5abd11be04c
bluke313 <bluke313@vt.edu>
2023-04-30 01:14:31 -0400

    added boundaries

--- BabyToGo/CharacterBody2D.gd
+++ BabyToGo/CharacterBody2D.gd
@@ -5,35 +5,43 @@ extends CharacterBody2D
 @export var max_y_speed = 1
 @export var upper_y_limit = -70
 @export var lower_y_limit = 300
+@export var stamina = 6000
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func get_y_velocity():
+	return 10
+
 func _process(delta):
-	if Input.is_action_pressed("UP"):
+	stamina -= 1
+	if Input.is_action_pressed("LEFT"):
 		var cur_rot = get_rotation()
 		set_rotation(clamp(cur_rot + deg_to_rad(-rot_amount), deg_to_rad(-rot_limit), deg_to_rad(rot_limit)))
-	if Input.is_action_pressed("DOWN"):
+	if Input.is_action_pressed("RIGHT"):
 		var cur_rot = get_rotation()
 		set_rotation(clamp(cur_rot + deg_to_rad(rot_amount), deg_to_rad(-rot_limit), deg_to_rad(rot_limit)))
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if stamina >= 0:
+		if position.y < -100:
+			var neg = clamp(get_rotation(), 0, rot_limit)
+			velocity.y = 11 * rad_to_deg(neg)
+		elif position.y > 800:
+			var pos = clamp(get_rotation(), -1 * rot_limit, 0)
+			velocity.y = 11 * rad_to_deg(pos)
+		else:
+			velocity.y = 11 * rad_to_deg(get_rotation())
+	else:
+		velocity.y += 5
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	velocity.x = SPEED
+	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
